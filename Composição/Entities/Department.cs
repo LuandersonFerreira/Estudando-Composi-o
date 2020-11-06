5bf2c28bbd40c6f47d495c06848a6d30def2c63d
Luanderson Ferreira <luandersonferreira123@gmail.com>
2020-11-06 08:32:05 -0300

    Projeto iniciado, com as entidades prontas

---
+++ Composição/Entities/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composição.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
